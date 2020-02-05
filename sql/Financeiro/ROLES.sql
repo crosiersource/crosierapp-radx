40a5545309b93b7b6a3db1763d9019ab31d28cca
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2020-02-05 11:03:31 -0300

    ...

--- sql/Financeiro/ROLES.sql
+++ sql/Financeiro/ROLES.sql
@@ -4,7 +4,9 @@ SET FOREIGN_KEY_CHECKS=0;
 
 
 INSERT INTO sec_role(id,inserted,updated,role,descricao,estabelecimento_id,user_inserted_id,user_updated_id) VALUES(null,now(),now(),'ROLE_FINAN','ROLE_FINAN',1,1,1);
+
 INSERT INTO sec_role(id,inserted,updated,role,descricao,estabelecimento_id,user_inserted_id,user_updated_id) VALUES(null,now(),now(),'ROLE_FINAN_ADMIN','ROLE_FINAN_ADMIN',1,1,1);
+
 
 
 COMMIT;
