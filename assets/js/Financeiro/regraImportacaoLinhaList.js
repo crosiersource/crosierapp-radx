4fe3ba28a7237508be722e9eed4290150026b65c
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2020-04-18 14:31:23 -0300

    ...

--- assets/js/Financeiro/regraImportacaoLinhaList.js
+++ assets/js/Financeiro/regraImportacaoLinhaList.js
@@ -4,6 +4,27 @@
 let listId = "#regraImportacaoLinhaList";
 
 
+class Utils {
+
+    static escapeHtml(string) {
+        let entityMap = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#39;',
+            '/': '&#x2F;',
+            '`': '&#x60;',
+            '=': '&#x3D;'
+        };
+
+        return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+            return entityMap[s];
+        });
+    }
+
+}
+
 function getDatatablesColumns() {
     return [
         {
