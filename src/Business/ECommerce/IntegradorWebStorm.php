83a2f71edec0bfaa3802dac135cf9b71c1df9d68
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2020-07-30 15:32:48 -0300

    ...

--- src/Business/ECommerce/IntegradorWebStorm.php
+++ src/Business/ECommerce/IntegradorWebStorm.php
@@ -1485,6 +1485,9 @@ class IntegradorWebStorm implements IntegradorBusiness
             $vendaItem->descricao = $produto->nome;
             $vendaItem->ordem = $ordem++;
             $vendaItem->devolucao = false;
+
+            $vendaItem->unidade = $produto->unidadePadrao;
+
             $vendaItem->precoVenda = $produtoWebStorm->valorUnitario->__toString();
             $vendaItem->qtde = $produtoWebStorm->quantidade->__toString();
             $vendaItem->subtotal = bcmul($vendaItem->precoVenda, $vendaItem->qtde, 2);
