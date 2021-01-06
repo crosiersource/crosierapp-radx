a62a2d764fd56f74d4a543a1dd68f4d8fcf7b41a
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-01-06 15:18:54 -0300

    ...

--- assets/js/Financeiro/movimentacaoForm_grupo.js
+++ assets/js/Financeiro/movimentacaoForm_grupo.js
@@ -8,6 +8,12 @@ import Moment from 'moment';
 
 import $ from "jquery";
 
+import 'select2/dist/css/select2.css';
+import 'select2';
+import 'select2/dist/js/i18n/pt-BR.js';
+import 'select2-bootstrap-theme/dist/select2-bootstrap.css';
+$.fn.select2.defaults.set("theme", "bootstrap");
+$.fn.select2.defaults.set("language", "pt-BR");
 
 import 'daterangepicker';
 
