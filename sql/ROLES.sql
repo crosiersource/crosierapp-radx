23b0be899cc12737c5a1b6d80472aca5777ac59c
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-01-19 23:52:49 -0300

    Arrumando SQLs

---
+++ sql/ROLES.sql
@@ -0,0 +1,54 @@
+START TRANSACTION;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_CRM', 'ROLE_CRM', 1, 1, 1);
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_CRM_ADMIN', 'ROLE_CRM_ADMIN', 1, 1, 1);
+
+
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_ESTOQUE', 'ROLE_ESTOQUE', 1, 1, 1);
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_ESTOQUE_ADMIN', 'ROLE_ESTOQUE_ADMIN', 1, 1, 1);
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_PEDIDOCOMPRA', 'ROLE_PEDIDOCOMPRA', 1, 1, 1);
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_PEDIDOCOMPRA_ADMIN', 'ROLE_PEDIDOCOMPRA_ADMIN', 1, 1, 1);
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_ROMANEIO', 'ROLE_ROMANEIO', 1, 1, 1);
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_ROMANEIO_ADMIN', 'ROLE_ROMANEIO_ADMIN', 1, 1, 1);
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_ESTOQUE_ECOMMERCE', 'ROLE_ESTOQUE_ECOMMERCE', 1, 1, 1);
+
+
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_FINAN', 'ROLE_FINAN', 1, 1, 1);
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_FINAN_ADMIN', 'ROLE_FINAN_ADMIN', 1, 1, 1);
+
+
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_FISCAL', 'ROLE_FISCAL', 1, 1, 1);
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_FISCAL_ADMIN', 'ROLE_FISCAL_ADMIN', 1, 1, 1);
+
+
+
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_RH', 'ROLE_RH', 1, 1, 1);
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_RH_ADMIN', 'ROLE_RH_ADMIN', 1, 1, 1);
+
+
+
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_VENDAS', 'ROLE_VENDAS', 1, 1, 1);
+INSERT INTO sec_role(id, inserted, updated, role, descricao, estabelecimento_id, user_inserted_id, user_updated_id)
+VALUES (null, now(), now(), 'ROLE_VENDAS_ADMIN', 'ROLE_VENDAS_ADMIN', 1, 1, 1);
+
+
+COMMIT;
