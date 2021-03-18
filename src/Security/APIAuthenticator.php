e74254a9f355037bb687220b441aa41fb4762a9d
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-03-18 17:06:06 -0300

    dev-master para @dev e APIAuthenticator.php correto

--- src/Security/APIAuthenticator.php
+++ src/Security/APIAuthenticator.php
@@ -3,7 +3,10 @@
 namespace App\Security;
 
 use CrosierSource\CrosierLibBaseBundle\Security\APIAuthenticatorTrait;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
 /**
@@ -15,4 +18,5 @@ class APIAuthenticator extends AbstractAuthenticator implements AuthenticationEn
 {
 
     use APIAuthenticatorTrait;
+
 }
