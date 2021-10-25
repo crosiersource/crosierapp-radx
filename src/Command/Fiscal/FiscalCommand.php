942dbb0095651e99520fd060939e58b20707e31e
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-10-25 15:24:49 -0300

    FiscalCommand->manifestarCienciaParaUltimas

--- src/Command/Fiscal/FiscalCommand.php
+++ src/Command/Fiscal/FiscalCommand.php
@@ -2,11 +2,14 @@
 
 namespace App\Command\Fiscal;
 
+use CrosierSource\CrosierLibBaseBundle\Business\Config\SyslogBusiness;
 use CrosierSource\CrosierLibBaseBundle\Exception\ViewException;
 use CrosierSource\CrosierLibRadxBundle\Business\Fiscal\DistDFeBusiness;
 use CrosierSource\CrosierLibRadxBundle\Business\Fiscal\NFeUtils;
+use CrosierSource\CrosierLibRadxBundle\Business\Fiscal\SpedNFeBusiness;
+use CrosierSource\CrosierLibRadxBundle\Entity\Fiscal\NotaFiscal;
+use CrosierSource\CrosierLibRadxBundle\EntityHandler\Fiscal\NotaFiscalEntityHandler;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -21,6 +24,12 @@ class FiscalCommand extends Command
     private NFeUtils $nfeUtils;
 
     private DistDFeBusiness $distDFeBusiness;
+
+    private SpedNFeBusiness $spedNFeBusiness;
+
+    private NotaFiscalEntityHandler $notaFiscalEntityHandler;
+    
+    private SyslogBusiness $syslog;
 
 
     /**
@@ -39,6 +48,34 @@ class FiscalCommand extends Command
     public function setDistDFeBusiness(DistDFeBusiness $distDFeBusiness): void
     {
         $this->distDFeBusiness = $distDFeBusiness;
+    }
+
+    /**
+     * @required
+     * @param SpedNFeBusiness $spedNFeBusiness
+     */
+    public function setSpedNFeBusiness(SpedNFeBusiness $spedNFeBusiness): void
+    {
+        $this->spedNFeBusiness = $spedNFeBusiness;
+    }
+
+    /**
+     * @required
+     * @param NotaFiscalEntityHandler $notaFiscalEntityHandler
+     */
+    public function setNotaFiscalEntityHandler(NotaFiscalEntityHandler $notaFiscalEntityHandler): void
+    {
+        $this->notaFiscalEntityHandler = $notaFiscalEntityHandler;
+    }
+
+
+    /**
+     * @required
+     * @param SyslogBusiness $syslog
+     */
+    public function setSyslog(SyslogBusiness $syslog): void
+    {
+        $this->syslog = $syslog->setApp('radx')->setComponent(self::class);
     }
 
 
@@ -70,6 +107,9 @@ class FiscalCommand extends Command
             case 'obterDistDFes':
                 $this->obterDistDFes($output);
                 break;
+            case 'manifestarCienciaParaUltimas':
+                $this->manifestarCienciaParaUltimas($output);
+                break;
             default:
                 throw new \RuntimeException('operacao desconhecida: ' . $operacao);
         }
@@ -79,10 +119,10 @@ class FiscalCommand extends Command
     /**
      * Chamar com:
      * - php bin/console crosierappradx:fiscal --operacao=obterDistDFes
-     * 
+     *
      * @param OutputInterface $output
      * @param int|null $primeiroNSU
-     * @throws \CrosierSource\CrosierLibBaseBundle\Exception\ViewException
+     * @throws ViewException
      */
     public function obterDistDFes(OutputInterface $output, int $primeiroNSU = null)
     {
@@ -101,6 +141,74 @@ class FiscalCommand extends Command
                 $output->writeln('OK');
             } catch (ViewException $e) {
                 $output->writeln('Erro ao obter e processar DistDFes para o CNPJ: ' . $cnpj);
+                $output->writeln($e->getMessage());
+            }
+            $output->writeln('----------');
+        }
+    }
+
+    /**
+     * Chamar com:
+     * - php bin/console crosierappradx:fiscal --operacao=manifestarCienciaParaUltimas
+     *
+     * @throws ViewException|\Doctrine\DBAL\Exception
+     */
+    public function manifestarCienciaParaUltimas(OutputInterface $output, int $primeiroNSU = null)
+    {
+
+        $rsDias = $this->nfeUtils->conn
+            ->fetchAssociative(
+                'SELECT valor FROM cfg_app_config WHERE chave = :chave AND app_uuid = :appUUID',
+                [
+                    'chave' => 'manifestarCienciaParaUltimas.dias',
+                    'appUUID' => $_SERVER['CROSIERAPPRADX_UUID']
+                ]);
+
+        $dias = $rsDias['valor'] ?? 3;
+        $cnpjs = $this->nfeUtils->getNFeConfigsCNPJs();
+
+        $repoNotaFiscal = $this->notaFiscalEntityHandler->getDoctrine()->getRepository(NotaFiscal::class);
+
+        foreach ($cnpjs as $cnpj) {
+            $msg = 'Manifestando "210210 - CIÊNCIA DA OPERAÇÃO" para o CNPJ: ' . $cnpj;
+            $this->syslog->info($msg);
+            $output->writeln($msg);
+            try {
+                $rsNFs = $this->nfeUtils->conn
+                    ->fetchAllAssociative(
+                        'SELECT id FROM fis_nf WHERE entrada_saida = \'E\' AND documento_destinatario = :cnpj AND resumo = true AND dt_emissao >= DATE(NOW()) - INTERVAL :dias DAY',
+                        [
+                            'cnpj' => $cnpj,
+                            'dias' => $dias
+                        ]);
+                
+                if ($rsNFs) {
+
+                    $msg = count($rsNFs) . ' NFs para manifestar nos últimos ' . $dias . ' dias para o CNPJ ' . $cnpj . '. Manifestando...';
+                    $output->writeln($msg);
+                    $this->syslog->info($msg);
+
+                    foreach ($rsNFs as $rNF) {
+                        try {
+                            $nf = $repoNotaFiscal->find($rNF);
+                            $this->spedNFeBusiness->manifestar($nf, 'ciencia');
+                            $msg = 'NF manifestada com sucesso (chave: ' . $nf->chaveAcesso . ')';
+                            $output->writeln($msg);
+                            $this->syslog->info($msg);
+                        } catch (ViewException $e) {
+                            $this->syslog->err('Ocorreu um erro ao manifestar a NF (chave: ' . $nf->chaveAcesso . ')');
+                        }
+                    }
+                    $output->writeln('OK');
+                } else {
+                    $msg = 'Nenhuma NF encontrada para o CNPJ ' . $cnpj;
+                    $this->syslog->info($msg);
+                    $output->writeln($msg);
+                }
+            } catch (ViewException $e) {
+                $msg = 'Erro - manifestarCienciaParaUltimas - CNPJ: ' . $cnpj;
+                $this->syslog->err($msg, $e->getMessage());
+                $output->writeln($msg);
                 $output->writeln($e->getMessage());
             }
             $output->writeln('----------');
