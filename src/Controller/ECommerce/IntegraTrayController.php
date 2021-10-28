cb825645c56bc42a8467991968d3c34176cd2bc7
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-10-28 17:50:14 -0300

    Integrações com a tray para defamilia

--- src/Controller/ECommerce/IntegraTrayController.php
+++ src/Controller/ECommerce/IntegraTrayController.php
@@ -4,6 +4,13 @@ namespace App\Controller\ECommerce;
 
 use CrosierSource\CrosierLibBaseBundle\Business\Config\SyslogBusiness;
 use CrosierSource\CrosierLibBaseBundle\Controller\BaseController;
+use CrosierSource\CrosierLibBaseBundle\Exception\ViewException;
+use CrosierSource\CrosierLibRadxBundle\Business\ECommerce\IntegradorTray;
+use CrosierSource\CrosierLibRadxBundle\Entity\Estoque\Depto;
+use CrosierSource\CrosierLibRadxBundle\Entity\Estoque\Produto;
+use Doctrine\DBAL\Connection;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,6 +23,8 @@ class IntegraTrayController extends BaseController
 
     protected SyslogBusiness $syslog;
 
+    protected IntegradorTray $integradorTray;
+
     /**
      * @required
      * @param SyslogBusiness $syslog
@@ -27,9 +36,22 @@ class IntegraTrayController extends BaseController
 
 
     /**
-     * @Route("/ecomm/tray/endpoint", name="ecomm_tray_endpoint")
+     * @required
+     * @param IntegradorTray $integradorTray
      */
-    public function trayEndpoint(Request $request): Response
+    public function setIntegradorTray(IntegradorTray $integradorTray): void
+    {
+        $this->integradorTray = $integradorTray;
+    }
+
+
+    /**
+     * Método exposto para receber o "code" da tray.
+     *
+     * @Route("/ecommIntegra/tray/endpoint", name="ecommIntegra_tray_endpoint")
+     * @throws ViewException
+     */
+    public function trayEndpoint(Connection $conn, Request $request)
     {
         $r = [];
         $r[] = 'Cliente IP: ' . $request->getClientIp();
@@ -37,29 +59,213 @@ class IntegraTrayController extends BaseController
         $r[] = '<hr />';
         $r[] = 'Content:';
         $r[] = $request->getContent();
-        $r[] = '<hr />';
+        $r[] = '--------------------------------';
         $r[] = 'Query';
         foreach ($request->query->all() as $k => $v) {
             $r[] = $k . ': ' . print_r($v, true);
         }
-
-        $r[] = '<hr />';
+        $r[] = '--------------------------------';
         $r[] = 'Request';
         foreach ($request->request->all() as $k => $v) {
             $r[] = $k . ': ' . print_r($v, true);
         }
-
-        $r[] = '<hr />';
+        $r[] = '--------------------------------';
         $r[] = 'Headers';
         foreach ($request->headers->all() as $k => $v) {
             $r[] = $k . ': ' . print_r($v, true);
         }
 
-        $r[] = '<hr />';
-
         $this->syslog->info('ecomm_tray_endpoint', implode(PHP_EOL, $r));
 
-        return new Response(implode('<br />', $r));
+        if ($request->get("code")) {
+            $storeId = $request->get('store');
+            $this->integradorTray->getStore($storeId);
+            $store['code'] = $request->get("code");
+            $this->integradorTray->saveStoreConfig($store);
+        } else {
+            return new Response('"code" não retornado pela tray');
+        }
     }
+
+
+    /**
+     * @Route("/ecommIntegra/tray/webhook", name="ecommIntegra_tray_webhook")
+     */
+    public function trayWebhook(Request $request): Response
+    {
+        $r = [];
+        $r[] = 'Cliente IP: ' . $request->getClientIp();
+        $r[] = 'Host: ' . $request->getHost();
+        foreach ($request->request->all() as $k => $v) {
+            $r[] = $k . ': ' . $v;
+        }
+        $this->syslog->info('tray/webhook', implode(PHP_EOL, $r));
+        return new Response('OK');
+    }
+
+
+    /**
+     * @Route("/api/ecommIntegra/tray/autorizarNaTray/{storeId}", name="api_ecommIntegra_tray_autorizarNaTray")
+     * @IsGranted("ROLE_ADMIN", statusCode=403)
+     * @throws ViewException
+     */
+    public function autorizarNaTray(?string $storeId = null): JsonResponse
+    {
+        $this->integradorTray->autorizarApp($storeId);
+
+        return new JsonResponse(
+            [
+                'RESULT' => 'OK',
+                'MSG' => 'Executado com sucesso',
+            ]
+        );
+    }
+
+
+    /**
+     * @Route("/api/ecommIntegra/tray/renewAccessTokenTray/{id}", name="ecommIntegra_clienteConfig_renewAccessTokenTray")
+     * @IsGranted("ROLE_ECOMM_ADMIN", statusCode=403)
+     * @throws ViewException
+     */
+    public function renewAccessTokenTray(?string $storeId = null): JsonResponse
+    {
+        $this->integradorTray->renewAccessToken($storeId);
+        return new JsonResponse(
+            [
+                'RESULT' => 'OK',
+                'MSG' => 'Executado com sucesso',
+            ]
+        );
+    }
+
+
+    /**
+     * @Route("/ecommIntegra/tray/integraCategoria/{depto}", name="ecommIntegra_tray_integraCategoria", requirements={"depto"="\d+"})
+     * @IsGranted("ROLE_ECOMM_ADMIN", statusCode=403)
+     * @throws ViewException
+     */
+    public function integraCategoria(Depto $depto): Response
+    {
+//        $integradorTray->endpoint = $clienteConfig->jsonData['url_loja'];
+//        $integradorTray->accessToken = $clienteConfig->jsonData['tray']['access_token'];
+//        $integradorTray->integraCategoria($depto);
+        return new Response('Depto/categoria integrado com sucesso');
+    }
+
+
+    /**
+     * @Route("/ecommIntegra/tray/integraProduto/{produto}", name="ecommIntegra_tray_integraProduto", requirements={"produto"="\d+"})
+     * @IsGranted("ROLE_ECOMM_ADMIN", statusCode=403)
+     * @throws ViewException
+     */
+    public function integraProduto(Produto $produto): Response
+    {
+//        $integradorTray->endpoint = $clienteConfig->jsonData['url_loja'];
+//        $integradorTray->accessToken = $clienteConfig->jsonData['tray']['access_token'];
+//        $integradorTray->integraProduto($produto);
+        return new Response('Produto integrado com sucesso');
+    }
+
+
+    /**
+     * @Route("/ecommIntegra/tray/integraVariacaoProduto/{produto}", name="ecommIntegra_tray_integraVariacaoProduto", requirements={"produto"="\d+"})
+     * @IsGranted("ROLE_ECOMM_ADMIN", statusCode=403)
+     * @throws ViewException
+     */
+    public function integraVariacaoProduto(Produto $produto): Response
+    {
+//        $integradorTray->endpoint = $clienteConfig->jsonData['url_loja'];
+//        $integradorTray->accessToken = $clienteConfig->jsonData['tray']['access_token'];
+//        $integradorTray->integraVariacaoProduto($produto);
+        return new Response('Variação de Produto integrado com sucesso');
+    }
+
+
+    /**
+     * @Route("/ecommIntegra/tray/obterPedido/{id}", name="ecommIntegra_tray_obterPedido", requirements={"id"="\d+"})
+     * @IsGranted("ROLE_ECOMM_ADMIN", statusCode=403)
+     */
+    public function obterPedido(int $numPedido): Response
+    {
+//        $integradorTray->endpoint = $clienteConfig->jsonData['url_loja'];
+//        $integradorTray->accessToken = $clienteConfig->jsonData['tray']['access_token'];
+//        $integradorTray->obterPedido($numPedido);
+        return new Response('Pedido integrado com sucesso');
+    }
+
+
+    /**
+     * @Route("/ecommIntegra/tray/integrarVendaParaECommerce/{numPedido}", name="ecommIntegra_tray_integrarVendaParaECommerce", requirements={"numPedido"="\d+"})
+     * @IsGranted("ROLE_ECOMM_ADMIN", statusCode=403)
+     * @throws ViewException
+     */
+    public function integrarVendaParaECommerce(int $numPedido): Response
+    {
+//        $integradorTray->endpoint = $clienteConfig->jsonData['url_loja'];
+//        $integradorTray->accessToken = $clienteConfig->jsonData['tray']['access_token'];
+//        $integradorTray->integrarVendaParaECommerce2($numPedido);
+        return new Response('Pedido integrado com sucesso');
+    }
+
+
+    /**
+     * @Route("/ecommIntegra/tray/atualizaDadosEnvio/{numPedido}", name="ecommIntegra_tray_atualizaDadosEnvio", requirements={"numPedido"="\d+"})
+     * @IsGranted("ROLE_ECOMM_ADMIN", statusCode=403)
+     * @throws ViewException
+     */
+    public function atualizaDadosEnvio(int $numPedido): Response
+    {
+//        $integradorTray->endpoint = $clienteConfig->jsonData['url_loja'];
+//        $integradorTray->accessToken = $clienteConfig->jsonData['tray']['access_token'];
+//        $integradorTray->atualizaDadosEnvio($numPedido);
+        return new Response('Pedido integrado com sucesso');
+    }
+
+
+    /**
+     * @Route("/ecommIntegra/tray/cancelarPedido/{numPedido}", name="ecommIntegra_tray_cancelarPedido", requirements={"numPedido"="\d+"})
+     * @IsGranted("ROLE_ECOMM_ADMIN", statusCode=403)
+     * @throws ViewException
+     */
+    public function cancelarPedido(int $numPedido): Response
+    {
+//        $integradorTray->endpoint = $clienteConfig->jsonData['url_loja'];
+//        $integradorTray->accessToken = $clienteConfig->jsonData['tray']['access_token'];
+//        $integradorTray->cancelarPedido($numPedido);
+        return new Response('Pedido cancelado com sucesso');
+    }
+
+
+    /**
+     * @Route("/ecommIntegra/tray/gerarNFeParaVenda/{codVenda}", name="ecommIntegra_tray_gerarNFeParaVenda")
+     * @IsGranted("ROLE_ESTOQUE_ADMIN", statusCode=403)
+     */
+    public function gerarNFeParaVenda(string $codVenda): Response
+    {
+        try {
+            $nfId = $this->integradorTray->gerarNFeParaVenda($codVenda);
+            return $this->redirectToRoute('fis_emissaonfe_form', ['id' => $nfId]);
+        } catch (\Throwable $e) {
+            $this->addFlash('error', $e->getMessage());
+        }
+        return new Response('ERRO');
+    }
+
+    /**
+     * @Route("/ecommIntegra/tray/integrarDadosFiscaisNoPedido/{codVenda}", name="ecommIntegra_tray_integrarDadosFiscaisNoPedido")
+     * @IsGranted("ROLE_ESTOQUE_ADMIN", statusCode=403)
+     */
+    public function integrarDadosFiscaisNoPedido(string $codVenda): Response
+    {
+        try {
+            $nfId = $this->integradorTray->integrarDadosFiscaisNoPedido($codVenda);
+            $this->addFlash('success', 'Dados fiscais integrados com sucesso no pedido');
+            return $this->redirectToRoute('fis_emissaonfe_form', ['id' => $nfId]);
+        } catch (\Throwable $e) {
+            $this->addFlash('error', $e->getMessage());
+        }
+        return new Response('ERRO');
+    }
+
 
 }
