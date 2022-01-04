0ad0b40a333a48d6fed81626cffc0671ce001de1
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2022-01-04 14:26:59 -0300

    Correção de bug com cálculo de subtotal e total na notaFiscalItem

--- assets/js/Fiscal/emissaoNFe/formItem.js
+++ assets/js/Fiscal/emissaoNFe/formItem.js
@@ -10,22 +10,24 @@ import 'jquery-maskmoney/dist/jquery.maskMoney.js';
 $(document).ready(function () {
 
     let $qtde = $('#nota_fiscal_item_qtde');
-    let $valorUnit = $('#nota_fiscal_item_valor_unit');
-    let $subTotal = $('#nota_fiscal_item_sub_total');
-    let $desconto = $('#nota_fiscal_item_valor_desconto');
-    let $valorTotal = $('#nota_fiscal_item_valor_total');
+    let $valorUnit = $('#nota_fiscal_item_valorUnit');
+    let $subTotal = $('#nota_fiscal_item_subtotal');
+    let $desconto = $('#nota_fiscal_item_valorDesconto');
+    let $valorTotal = $('#nota_fiscal_item_valorTotal');
 
     function resValorTotal() {
+      console.log('resValorTotal');
         let qtde = $qtde.maskMoney('unmasked')[0];
         let valorUnit = $valorUnit.maskMoney('unmasked')[0];
 
         let subTotal = (qtde * valorUnit);
         $subTotal.val(subTotal.toFixed(2).replace('.',',')).maskMoney('mask');
 
-        let desconto = $desconto.maskMoney('unmasked')[0];
+        let desconto = $desconto.maskMoney('unmasked')[0] ?? 0.00;
         let valorTotal = (subTotal - desconto).toFixed(2).replace('.',',');
         $valorTotal.val(valorTotal).maskMoney('mask');
         CrosierMasks.maskDecs();
+        console.log('fim');
     }
 
     $qtde.blur(function () {
