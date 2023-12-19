e70a922f1009373a82cf7d831cdcac518031cfbc
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2023-12-19 15:59:40 -0300

    ajustar_doctrine_migrations.sql

--- migrations/ajustar_doctrine_migrations.sql
+++ migrations/ajustar_doctrine_migrations.sql
@@ -1,3 +1,5 @@
 INSERT INTO doctrine_migration_versions(version, executed_at, execution_time) VALUES ('DoctrineMigrations\\VersionRadx001',now(),1);
 INSERT INTO doctrine_migration_versions(version, executed_at, execution_time) VALUES ('DoctrineMigrations\\VersionRadx002',now(),1);
 INSERT INTO doctrine_migration_versions(version, executed_at, execution_time) VALUES ('DoctrineMigrations\\VersionRadx003',now(),1);
+INSERT INTO doctrine_migration_versions(version, executed_at, execution_time) VALUES ('DoctrineMigrations\\VersionRadx004',now(),1);
+INSERT INTO doctrine_migration_versions(version, executed_at, execution_time) VALUES ('DoctrineMigrations\\VersionRadx005',now(),1);
